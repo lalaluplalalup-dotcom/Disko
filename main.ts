23f66c539527365ed2cc9c17031c535f8d8d2e5a
lalaluplalalup-dotcom <lalaluplalalup@gmail.com>
2026-01-19 15:07:51 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,51 @@
+let strip: neopixel.Strip = null
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    strip.showColor(neopixel.colors(NeoPixelColors.White))
+    strip.show()
+})
+basic.forever(function () {
+    strip.showColor(neopixel.colors(NeoPixelColors.White))
+    strip.showRainbow(1, 5000)
+    strip.show()
+    strip.show()
+    basic.showString("party")
+    basic.showIcon(IconNames.Fabulous)
+    strip.setBrightness(2000)
+    strip.showColor(neopixel.colors(NeoPixelColors.White))
+    strip.show()
+    basic.pause(1000)
+    strip.showColor(neopixel.colors(NeoPixelColors.Green))
+    strip.show()
+    basic.pause(1000)
+    strip.showColor(neopixel.colors(NeoPixelColors.Blue))
+    strip.show()
+    basic.pause(1000)
+    strip.showColor(neopixel.colors(NeoPixelColors.Black))
+    strip.show()
+    basic.pause(1000)
+    if (input.soundLevel() >= 31) {
+        strip.showRainbow(1, 50000)
+        for (let index = 0; index < 6000; index++) {
+            strip.rotate(1)
+            strip.show()
+            basic.pause(5)
+        }
+        for (let index = 0; index < 10; index++) {
+            strip.showColor(neopixel.colors(NeoPixelColors.Orange))
+            strip.show()
+            basic.pause(50)
+            strip.showColor(neopixel.colors(NeoPixelColors.Blue))
+            strip.show()
+            basic.pause(50)
+            strip.showColor(neopixel.colors(NeoPixelColors.Yellow))
+            strip.show()
+            basic.pause(50)
+            strip.showColor(neopixel.colors(NeoPixelColors.Green))
+            strip.show()
+            basic.pause(50)
+            strip.showColor(neopixel.colors(NeoPixelColors.Purple))
+            strip.show()
+            basic.pause(50)
+        }
+    }
+})
